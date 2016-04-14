bb4adeb8059b51b2fb2d23bac2e2dffab4ec65d3
Philipp <philipp.aster@hallst.com>
2016-04-14 13:46:52 +0200

    modified kwikr.js

--- kwikr.js
+++ kwikr.js
@@ -11,41 +11,48 @@ if(process.argv.length > 2) {
     // Read the first additional argument passed to the program
     var projectName = process.argv[3];
     var sourceDir = projectName+"/src";
+    if (process.argv[2] === "new"){
+        if (!fs.existsSync(projectName)){
+                fs.mkdirSync(projectName);
+                //fs.mkdirSync(sourceDir);
+        }
+        else{
+            console.log("A Project named " + projectName + " already exists. Please run the command again with another project name.");
+            process.exit()
+        }
 
-    if (!fs.existsSync(projectName)){
-            fs.mkdirSync(projectName);
-            //fs.mkdirSync(sourceDir);
+        // get input from the user.
+        var description = prompt('Project Description? ');
+        var keywords = prompt('Project Keywords? ');
+        var author = prompt('Author Name? ');
+        var settings = projectName + '/settings.json'
+        var obj = {
+            project_name: projectName,
+            project_author: author,
+            project_description: description,
+            project_keywords: keywords
+        }
+
+        // write input to settings.json
+        jsonfile.writeFile(settings, obj, {spaces: 2}, function(err) {
+            if (err) {
+                console.error(err);
+            }
+        })
+
+        //Copy src files
+        fs.copy('lib', projectName, function (err) {
+            if (err) {
+                console.error(err);
+            } else {
+                console.log("Congratulations, you succesfully created " + projectName);
+            }
+        });
     }
     else{
-        console.log("A Project named " + projectName + " already exists. Please run the command again with another project name.");
-        process.exit()
+        console.log("Wrong command... Run 'kwikr new <Project_Name>'");
     }
 
-    // get input from the user.
-    var description = prompt('Project Description? ');
-    var keywords = prompt('Project Keywords? ');
-    var author = prompt('Author Name? ');
-    var settings = projectName + '/settings.json'
-    var obj = {
-        project_name: projectName,
-        project_author: author,
-        project_description: description,
-        project_keywords: keywords
-    }
-
-    // write input to settings.json
-    jsonfile.writeFile(settings, obj, {spaces: 2}, function(err) {
-        console.error(err)
-    })
-
-    //Copy src files
-    fs.copy('lib', projectName, function (err) {
-        if (err) {
-            console.error(err);
-        } else {
-            console.log("success!");
-        }
-    });
 
 
 } else {
